d22b30d2c755775ec857a45abc863521fb230322
josue <josue.edardo206@gmail.com>
2013-10-03 23:25:30 -0300

    second commit

--- src/gitsample/GitSample.java
+++ src/gitsample/GitSample.java
@@ -16,7 +16,7 @@ public class GitSample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //second commit
     }
     
 }
